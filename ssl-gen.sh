cd637655ac44a4ed2b926e68b6885dff38b4cbce
Anuwong Mongkudkornburee <anuwong.mon@truedigital.com>
2024-03-23 01:02:18 +0700

    chore: nginx cert ssl

--- ssl-gen.sh
+++ ssl-gen.sh
@@ -1,4 +1,5 @@
-!#/bin/sh
+#!/bin/sh
 
 # Generate a self-signed certificate for the server
-openssl req -new -x509 -days 3650 -nodes -out server.crt -keyout server.key
+# openssl req -new -x509 -days 3650 -nodes -out server.crt -keyout server.key
+sudo openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout server.key -out server.crt
