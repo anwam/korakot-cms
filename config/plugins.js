8992ac594e1a2dfd2aabd18595bc1cd9cd2c5018
anwam <dev.anuwong@gmail.com>
2025-09-19 08:32:12 +0700

    Implement code changes to enhance functionality and improve performance

--- config/plugins.js
+++ config/plugins.js
@@ -25,39 +25,23 @@ module.exports = ({ env }) => ({
       delete: {},
     },
   },
-  'strapi-cache': {
-    enabled: true,
+  "rest-cache": {
     config: {
-      debug: false, // Enable debug logs
-      max: 1000, // Maximum number of items in the cache (only for memory cache)
-      ttl: 1000 * 60 * 60, // Time to live for cache items (1 hour)
-      size: 1024 * 1024 * 1024, // Maximum size of the cache (1 GB) (only for memory cache)
-      allowStale: false, // Allow stale cache items (only for memory cache)
-      cacheableRoutes: ['/api/products', '/api/categories'], // Caches routes which start with these paths (if empty array, all '/api' routes are cached)
-      provider: 'memory', // Cache provider ('memory' or 'redis')
-      cacheHeaders: true, // Plugin also stores response headers in the cache (set to false if you don't want to cache headers)
-      cacheAuthorizedRequests: false, // Cache requests with authorization headers (set to true if you want to cache authorized requests)
-      cacheGetTimeoutInMs: 1000, // Timeout for getting cached data in milliseconds (default is 1 seconds)
-    }
+      provider: {
+        name: "memory",
+        options: {
+          max: 32767,
+          maxAge: 3600,
+        },
+      },
+      strategy: {
+        contentTypes: [
+          // list of Content-Types UID to cache
+          "api::car.car",
+        ],
+      },
+    },
   },
-
-  // "rest-cache": {
-  //   config: {
-  //     provider: {
-  //       name: "memory",
-  //       options: {
-  //         max: 32767,
-  //         maxAge: 3600,
-  //       },
-  //     },
-  //     strategy: {
-  //       contentTypes: [
-  //         // list of Content-Types UID to cache
-  //         "api::car.car",
-  //       ],
-  //     },
-  //   },
-  // },
   meilisearch: {
     config: {
       host: env("MEILISEARCH_URL") || "http://meilisearch:7700",
